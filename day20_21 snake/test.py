cde1a554fe33f0a56614738f8425765eac6ffed8
Brendan <brendanmurrayfl@gmail.com>
2021-01-10 17:59:24 +0000

    Rename day 20

---
+++ day20_21 snake/test.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+tim.penup()
+tim.hideturtle()
+tim.color('white')
+tim.write("Hello world", True, 'center')
+
+
+
+screen.exitonclick()
