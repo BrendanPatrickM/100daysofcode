2c73b3c08255857e9d18e274d0f232a29038ad5e
Brendan <brendanmurrayfl@gmail.com>
2021-01-18 23:16:47 +0000

    rename folders to describe daily challenge

--- day25_csv_pandas/testing_pandas.py
+++ day25_csv_pandas/testing_pandas.py
@@ -1,12 +1,13 @@
 # import csv
-# with open ('/Users/brendan/Documents/Python/100daysofcode/day25/data.csv', mode='r') as weather_file:  
+# with open ('/Users/brendan/Documents/Python/100daysofcode/day25/data.csv',
+#             mode='r') as weather_file:
 #     data = csv.reader(weather_file)
 #     temperatures = []
 #     for row in data:
 #         if row [1]!= 'temp':
 #             new_temp = int(row[1])
 #             temperatures.append(new_temp)
-        
+
 # print(temperatures)
 
 import pandas
@@ -15,19 +16,18 @@ temp_list = data['temp'].to_list()
 # print(temp_list)
 
 # # average_temp = sum(temp_list)/len(temp_list)
-# # print(average_temp) 
+# # print(average_temp)
 # average_temp = data['temp'].mean()
 # max_temp = data['temp'].max()
-# print (f'Temperature:\n average:{round(average_temp)}\n high temp: {max_temp} ' )
+# print(f'Temperature:\n avg:{round(average_temp)}\n high temp: {max_temp}')
 
 # max_temp = data['temp'].max()
 # print(data[data.temp == max_temp])
 
 
 def convert(temp):
-    f  = temp * 2 +30
+    f = temp * 2 + 30
     return f
-    
+
+
 print(convert(data[data.day == 'Monday'].temp))
-
-
