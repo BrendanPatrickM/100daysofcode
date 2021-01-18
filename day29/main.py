4438740e94bdb65eba681254e3f8c144e1544194
Brendan <brendanmurrayfl@gmail.com>
2021-01-18 13:34:05 +0000

    add search function to password manager

--- day29/main.py
+++ day29/main.py
@@ -7,14 +7,17 @@ STARTING_VALUE = 'brendandotm@gmail.com'
 
 
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    random_letter = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    random_let = [random.choice(letters) for _ in range(random.randint(8, 10))]
     random_num = [random.choice(numbers) for _ in range(random.randint(2, 4))]
     random_sym = [random.choice(symbols) for _ in range(random.randint(2, 4))]
-    password_list = random_letter + random_num + random_sym
+    password_list = random_let + random_num + random_sym
     random.shuffle(password_list)
     password = ''.join(password_list)
     pass_ent.insert(0, password)
@@ -24,13 +27,14 @@ def generate_password():
 def layout():
     canvas.grid(column=1, row=0)
     web_label.grid(column=0, row=1)
-    web_ent.grid(column=1, row=1, columnspan=2)
+    web_ent.grid(column=1, row=1)
     use_label.grid(column=0, row=2)
     use_ent.grid(column=1, row=2, columnspan=2)
     pass_label.grid(column=0, row=3)
     pass_ent.grid(column=1, row=3)
     gen_but.grid(column=2, row=3)
     add_but.grid(column=1, row=4, columnspan=2)
+    search_but.grid(column=2, row=1)
 
 
 def save():
@@ -61,6 +65,25 @@ def save():
         clear_fields()
 
 
+def search():
+    website = web_ent.get()
+    try:
+        with open('data.json', mode='r') as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(message='No data file found')
+        pass
+
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(message=f'{website}\n{email}\n{password}')
+        else:
+            messagebox.showinfo(message=f'No details for {website} exist')
+
+
 def clear_fields():
     web_ent.delete(0, len(web_ent.get()))
     pass_ent.delete(0, len(pass_ent.get()))
@@ -79,14 +102,16 @@ web_label = tkinter.Label(text='Website:')
 use_label = tkinter.Label(text='Email/Username:')
 pass_label = tkinter.Label(text='Password:')
 # ENTRIES
-web_ent = tkinter.Entry(width=34)
+web_ent = tkinter.Entry(width=19)
 web_ent.focus()
 use_ent = tkinter.Entry(width=34)
 use_ent.insert(0, STARTING_VALUE)
 pass_ent = tkinter.Entry(width=19)
 # BUTTONS
-gen_but = tkinter.Button(text='Generate Password', width=11, command=generate_password)
+gen_but = tkinter.Button(text='Generate Password', width=11,
+                         command=generate_password)
 add_but = tkinter.Button(text='Add', width=32, command=save)
+search_but = tkinter.Button(text='Search', width=11, command=search)
 
 layout()
 window.mainloop()
