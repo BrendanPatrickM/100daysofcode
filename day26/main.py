9b6861006f423e133c96b0b4636682905db1e165
Brendan <brendanmurrayfl@gmail.com>
2021-01-18 11:33:25 +0000

    add exception handling to nato alphabet translater

--- day26/main.py
+++ day26/main.py
@@ -1,8 +1,21 @@
 import pandas
 
-nato_data_frame = pandas.read_csv('nato_phonetic_alphabet.csv')
-nato_dict = {row.letter: row.code for (index, row) in nato_data_frame.iterrows()}
 
-word_to_translate = input('Enter word to translate: ').upper()
-spelling_list = [nato_dict[letter] for letter in word_to_translate]
-print(spelling_list)
+def translate():
+    word_to_translate = input('Enter word to translate: ').upper()
+    spelling_list = [nato_dict[letter] for letter in word_to_translate]
+    print(spelling_list)
+
+
+nato_data_frame = pandas.read_csv('nato_phonetic_alphabet.csv')
+nato_dict = {row.letter: row.code for (index, row) in
+             nato_data_frame.iterrows()}
+
+complete = False
+while not complete:
+    try:
+        translate()
+    except KeyError:
+        print('Sorry, please only use letters of the alphabet')
+    else:
+        complete = True
