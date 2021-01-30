ca035b1974cec4a46611742dcb19e0519a766659
Brendan <brendanmurrayfl@gmail.com>
2021-01-30 13:21:49 +0000

    add twilio call to rain check

--- day35_apikeys/rain_alert/main.py
+++ day35_apikeys/rain_alert/main.py
@@ -1,5 +1,9 @@
 import requests
-api_key = '0a97b34b3262c74cf35dd63d420c4564'
+import os
+from twilio.rest import Client
+api_key = os.environ.get('OWM_API_KEY')
+account_sid = 'ACa79b1277a779b09d55d4abfaf912b7ba'
+auth_token = os.environ.get('AUTH_TOKEN')
 lat = 52.844349
 lon = -8.983000
 
@@ -16,13 +20,24 @@ response.raise_for_status()
 print(response.status_code)
 data = response.json()
 slice_12h = data['hourly'][:12]
+print(slice_12h)
 
 will_rain = False
 for condition in slice_12h:
     if condition['weather'][0]['id'] < 700:
         will_rain = True
+
 if will_rain:
-    print("don't forget to bring an umbrella")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+                    .create(
+                        body="It's going to rain ☔️",
+                        from_='+14078907419',
+                        to='+447453584873'
+                    )
+
+    print(message.status)
+
 
 # conditions = []
 # for n in range(0,12):
