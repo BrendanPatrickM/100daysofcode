9a6c4f85a4008698aaa2e8145f8ccb614cd3fddc
Brendan <brendanmurrayfl@gmail.com>
2021-02-01 20:42:57 +0000

    mvp commit

--- day38_workout_tracker/main.py
+++ day38_workout_tracker/main.py
@@ -2,7 +2,6 @@ import os
 import requests
 from datetime import datetime
 
-date = (datetime.now()).strftime('%d/%m/%Y')
 GENDER = 'male'
 WEIGHT = 75
 HEIGHT = 173
@@ -10,9 +9,9 @@ AGE = 35
 APP_ID = os.environ.get('APP_ID')
 APP_KEY = os.environ.get('APP_KEY')
 
-
 nutrix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-# plain_text = input('Tell me which excercise you did?: ')
+sheetly_endpoint = 'https://api.sheety.co/9b32cef2ac94d8b0c8a576aafe17f209/workoutTracker/workouts'
+plain_text = input('Tell me which excercise you did?: ')
 
 headers = {
     'x-app-id': APP_ID,
@@ -25,8 +24,22 @@ params = {
     'gender': GENDER,
     'weight_kg': WEIGHT,
     'height_cm': HEIGHT,
-    'age':AGE,
+    'age': AGE,
 }
 
-# response = requests.post(url=nutrix_endpoint, headers=headers, json=params)
-# response =response.json()
+response = requests.post(url=nutrix_endpoint, headers=headers, json=params)
+response = response.json()
+
+for exercise in response['exercises']:
+    sheet_params = {
+        "workout": {
+            "date": (datetime.now()).strftime('%d/%m/%Y'),
+            'time': datetime.now().strftime("%X"),
+            'exercise': exercise['name'].title(),
+            'duration': exercise['duration_min'],
+            'calories': exercise['nf_calories'],
+        }
+    }
+    sheet_response = requests.post(url=sheetly_endpoint, json=sheet_params)
+    sheet_response = sheet_response.json()
+    print (sheet_response)
