38066a9423c8cf0eddc3606ed619d6b29c880e1a
Brendan <brendanmurrayfl@gmail.com>
2021-02-02 18:20:28 +0000

    implement data manager  class

--- day39_capstone_flight tracker/main.py
+++ day39_capstone_flight tracker/main.py
@@ -3,20 +3,12 @@ from flight_search import FlightSearch
 from pprint import pprint
 
 
-# data = DataManager()
-# sheet_data = data.get_data()
-sheet_data = [{'city': 'Paris', 'iataCode': '', 'id': 2, 'lowestPrice': 54},
- {'city': 'Berlin', 'iataCode': '', 'id': 3, 'lowestPrice': 42},
- {'city': 'Tokyo', 'iataCode': '', 'id': 4, 'lowestPrice': 485},
- {'city': 'Sydney', 'iataCode': '', 'id': 5, 'lowestPrice': 551},
- {'city': 'Istanbul', 'iataCode': '', 'id': 6, 'lowestPrice': 95},
- {'city': 'Kuala Lumpur', 'iataCode': 'testcode', 'id': 7, 'lowestPrice': 414},
- {'city': 'New York', 'iataCode': '', 'id': 8, 'lowestPrice': 240},
- {'city': 'San Francisco', 'iataCode': '', 'id': 9, 'lowestPrice': 260},
- {'city': 'Cape Town', 'iataCode': '', 'id': 10, 'lowestPrice': 378}]
+data = DataManager()
+sheet_data = data.get_data()
 
 
 flightsearch = FlightSearch()
+
 for destination in sheet_data:
     code = destination['iataCode']
     city = destination['city']
@@ -26,6 +18,4 @@ for destination in sheet_data:
         destination['iataCode'] = new_iata
 
 
-pprint(sheet_data)
- 
-
+data.update_iata(sheet_data)
