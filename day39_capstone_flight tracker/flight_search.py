12b034b7d5d51b7b9607c96a7e7e81f6ff50e45c
Brendan <brendanmurrayfl@gmail.com>
2021-02-03 13:19:02 +0000

    initial flight search function

--- day39_capstone_flight tracker/flight_search.py
+++ day39_capstone_flight tracker/flight_search.py
@@ -1,8 +1,31 @@
+import requests
+import os
+from pprint import pprint
+TEQUILA_EP = 'https://tequila-api.kiwi.com'
+API_KEY = os.environ.get('TEQUILA_KEY')
+
+
 class FlightSearch:
     def __init__(self):
-        pass    
-    
-    def fetch_code(self, city):
-        return ('testing')
+        self.headers = {"apikey": API_KEY}
 
-        
+    def fetch_code(self, city):
+        endpoint = f'{TEQUILA_EP}/locations/query'
+        # headers = {"apikey": API_KEY}
+        query = {'term': city, 'location_types': 'city'}
+        response = requests.get(url=endpoint, params=query, headers=self.headers)
+        reply = response.json()
+        return reply['locations'][0]['code']
+
+    def find_flights(self):
+        print("finding flights")
+        endpoint = f'{TEQUILA_EP}/search'
+        query = {
+            'fly_from': 'LON',
+            'fly_to': 'IE',
+            'date_from': '01/04/2021',
+            'date_to': '05/06/2021',
+        }
+        response = requests.get(url=endpoint, params=query, headers=headers)
+        pprint(response.text)
+
