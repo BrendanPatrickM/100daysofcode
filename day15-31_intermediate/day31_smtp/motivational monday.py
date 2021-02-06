dd4fde0fd4f10d265faabac3f314475d5de98fc1
Brendan <brendanmurrayfl@gmail.com>
2021-02-06 11:33:26 +0000

    move files

---
+++ day15-31_intermediate/day31_smtp/motivational monday.py
@@ -0,0 +1,25 @@
+import datetime as dt
+import random
+import smtplib
+
+my_email = ''
+my_password = ''
+
+
+with open('day31_smtp/quotes.txt', 'r') as quote_file:
+    quotes = quote_file.readlines()
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday == 0:
+    quote_num = random.randint(0, len(quotes))
+
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=my_email,
+            msg=f"Subject: Motivational Monday\n\n{quotes[quote_num]}"
+        )
