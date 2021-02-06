ae035460257b161ae846945377dee9d3b613f724
Brendan <brendanmurrayfl@gmail.com>
2021-02-06 11:33:50 +0000

    move files delete

--- day20_21 snake/test.py
+++
@@ -1,4 +0,0 @@
-with open('data.txt', mode = 'r') as data_file:
-    a = data_file.read()
-print('hello')
-print(a)
--- day27_tkinter/test.py
+++
@@ -1,5 +0,0 @@
-def find_short(s):
-    l =  min(s.split(), key=len)
-    return l # l: shortest word length
-
-print(find_short("bitcoin take over the world maybe who knows perhaps"))
