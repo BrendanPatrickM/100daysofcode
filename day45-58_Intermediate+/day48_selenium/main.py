86bde1b461579a7be3aee575b7c46960e2ae21e5
Brendan <brendanmurrayfl@gmail.com>
2021-02-12 16:48:14 +0000

    challenge 1 commit

--- day45-58_Intermediate+/day48_selenium/main.py
+++ day45-58_Intermediate+/day48_selenium/main.py
@@ -6,9 +6,10 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get('https://www.python.org')
 events_tag = driver.find_elements_by_css_selector('.medium-widget.event-widget .shrubbery ul li')
 
-events = [item.text for item in events_tag]
+events = [item.text.split('\n') for item in events_tag]
+event_dict = {entry[0]:entry[1] for entry in events}
 print(events)
+print(event_dict)
+
 
 driver.quit()
-
-
