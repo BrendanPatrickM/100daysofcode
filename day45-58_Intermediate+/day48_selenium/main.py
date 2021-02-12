7ceddeef3a578fb4e6b57f8d8b7a9a449b5d95d6
Brendan <brendanmurrayfl@gmail.com>
2021-02-12 16:49:20 +0000

    PEP 8

--- day45-58_Intermediate+/day48_selenium/main.py
+++ day45-58_Intermediate+/day48_selenium/main.py
@@ -4,10 +4,11 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 
 driver.get('https://www.python.org')
-events_tag = driver.find_elements_by_css_selector('.medium-widget.event-widget .shrubbery ul li')
+events_tag = driver.find_elements_by_css_selector(
+    '.medium-widget.event-widget .shrubbery ul li')
 
 events = [item.text.split('\n') for item in events_tag]
-event_dict = {entry[0]:entry[1] for entry in events}
+event_dict = {entry[0]: entry[1] for entry in events}
 print(events)
 print(event_dict)
 
