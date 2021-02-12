60872fea7573344a7bfd90d1883a8168d9e86e1b
Brendan <brendanmurrayfl@gmail.com>
2021-02-12 16:18:00 +0000

    selenium working and setup on client

--- day45-58_Intermediate+/day48_selenium/main.py
+++ day45-58_Intermediate+/day48_selenium/main.py
@@ -3,6 +3,12 @@ chrome_driver_path = "/Users/brendan/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 
-driver.get('https://www.amazon.com')
-# driver.close()
+driver.get('https://www.python.org')
+events_tag = driver.find_elements_by_css_selector('.medium-widget.event-widget .shrubbery ul li')
+
+events = [item.text for item in events_tag]
+print(events)
+
 driver.quit()
+
+
