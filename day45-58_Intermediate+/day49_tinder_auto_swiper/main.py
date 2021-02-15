bbab3bd2a1a0668b04c9c155b37095de8b6c58ba
Brendan <brendanmurrayfl@gmail.com>
2021-02-15 16:59:53 +0000

    biref completed - todo, error handling for superlike window

--- day45-58_Intermediate+/day49_tinder_auto_swiper/main.py
+++ day45-58_Intermediate+/day49_tinder_auto_swiper/main.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+
 import os
 
 EMAIL = os.environ.get('EMAIL')
 PASSWORD = os.environ.get('PASS')
-
 
 path = '/Users/brendan/Development/chromedriver'
 driver = webdriver.Chrome(executable_path=path)
@@ -22,16 +23,46 @@ base_window = driver.window_handles[0]
 login_popup = driver.window_handles[1]
 driver.switch_to.window(login_popup)
 
+sleep(1)
 email_field = driver.find_element_by_name('email')
 password_field = driver.find_element_by_name('pass')
 email_field.send_keys(EMAIL)
 password_field.send_keys(PASSWORD)
 password_field.send_keys(Keys.ENTER)
 
-driver.switch_to(base_window)
+print(driver.window_handles)
+driver.switch_to.window(base_window)
+print(driver.title)
+
 
 # clear notifications
-location = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
-location.click()
-noification_no = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
-noification_no.click()
+sleep(7)
+allow_location_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+allow_location_button.click()
+cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+cookies.click()
+notifications_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+notifications_button.click()
+sleep(2)
+
+# click like
+
+for n in range(10):
+
+    # Add a 1 second delay between likes.
+    sleep(1)
+
+    try:
+        print("called")
+        like_button = driver.find_element_by_xpath(
+            '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+        like_button.click()
+
+    except ElementClickInterceptedException:
+        try:
+            match_popup = driver.find_element_by_css_selector(".itsAMatch a")
+            match_popup.click()
+
+        # Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
+        except NoSuchElementException:
+            sleep(2)
