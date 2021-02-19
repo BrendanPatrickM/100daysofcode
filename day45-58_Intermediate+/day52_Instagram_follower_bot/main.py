b71008be2ed0589a258fbed358acb4e8fe99b10b
Brendan <brendanmurrayfl@gmail.com>
2021-02-19 13:51:21 +0000

    create credentials

---
+++ day45-58_Intermediate+/day52_Instagram_follower_bot/main.py
@@ -0,0 +1,6 @@
+import os
+
+DRIVER_PATH = '/Users/brendan/Development/chromedriver'
+INSTA_USER = os.environ.get('INSTAUSER')
+INSTA_PASS = os.environ.get('PASSWORD')
+SIMILAR_ACCOUNT = 'https://www.instagram.com/dji_official/'
