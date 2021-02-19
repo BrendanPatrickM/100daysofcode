beadffcce5355ab67846008a8d5112ed1aa1337c
Brendan <brendanmurrayfl@gmail.com>
2021-02-19 13:55:09 +0000

    frame class

--- day45-58_Intermediate+/day52_Instagram_follower_bot/main.py
+++ day45-58_Intermediate+/day52_Instagram_follower_bot/main.py
@@ -4,3 +4,16 @@ DRIVER_PATH = '/Users/brendan/Development/chromedriver'
 INSTA_USER = os.environ.get('INSTAUSER')
 INSTA_PASS = os.environ.get('PASSWORD')
 SIMILAR_ACCOUNT = 'https://www.instagram.com/dji_official/'
+
+class InstaFollower():
+    def __init__(self):
+        pass
+
+    def login(self):
+        pass
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
