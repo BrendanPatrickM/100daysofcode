02a75b83b57226c091d3946a278f9a1b6eed6317
Brendan <brendanmurrayfl@gmail.com>
2021-02-20 14:28:51 +0000

    initial commit

---
+++ day45-58_Intermediate+/day55_flask/main.py
@@ -0,0 +1,27 @@
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!!!'
+
+from time import sleep
+
+
+def delay_decorator(function):
+    def wrapper():
+        sleep(2)
+        function()
+        function()
+        function()
+    return wrapper
+
+
+@delay_decorator
+def hello():
+    print('hello')
+
+
+
+
+hello()
